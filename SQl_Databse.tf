069e25d69f9de79bdc3d2978519c5a11f85430d7
khatnanimuskan <52534858+khatnanimuskan@users.noreply.github.com>
2019-08-29 18:38:23 +0530

    Delete SQl_Databse.tf

--- SQl_Databse.tf
+++
@@ -1,8 +0,0 @@
-resource "azurerm_sql_database" "SD" {
-  name                = "${element(var.sql_database,count.index)}"
-  resource_group_name = "${azurerm_resource_group.RG-1.name}"
-  location            = "${var.location}"
-  server_name         = "${element(var.sql_database,count.index)}"
-  server_name         = "${element(azurerm_sql_server.sql-1.*.name,count.index)}" 
-  count = "${var.countDB}"
-}
