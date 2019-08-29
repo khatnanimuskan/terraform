d3e2c8767710f64b12538fcfa34803ab74d4072f
khatnanimuskan <bcaitms16.muskan@poornima.edu.in>
2019-08-29 17:29:46 +0530

    newcommittt

--- terra/RG-1.tf
+++ terra/RG-1.tf
@@ -6,7 +6,7 @@ variable "location" {
 default ="Central US"
 }
 
-resource "azurerm_resource_group" "RG" {
+resource "azurerm_resource_group" "RG-23" {
 name = "${var.resource_group}" 
 location = "${var.location}"
 }
