61f1f059177aec06d979d5907a026db9a16819b2
khatnanimuskan <52534858+khatnanimuskan@users.noreply.github.com>
2019-08-29 18:37:00 +0530

    Delete SQL_DataWarehouse.tf

--- SQL_DataWarehouse.tf
+++
@@ -1,9 +0,0 @@
-
- resource "azurerm_sql_warehouse" "SW" {
-  name                = "${element(var.sql-warehouse,count.index)}"
-  resource_group_name = "${azurerm_resource_group.RG-1.name}"
-  location            = "${var.location}"
-  server_name         = "${element(var.sql-warehouse,count.index)}"
-  server_name         = "${element(azurerm_sql_server.sql-1.*.name,count.index)}" 
-  count =  "${var.countDW}"
-}
