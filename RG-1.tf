2444639a744f927c68fbe587059385e2f98301c0
khatnanimuskan <52534858+khatnanimuskan@users.noreply.github.com>
2019-12-17 16:51:36 +0530

    Update RG-1.tf

--- RG-1.tf
+++ RG-1.tf
@@ -10,3 +10,11 @@ resource "azurerm_resource_group" "RG-1" {
 name = "${var.resource_group}" 
 location = "${var.location}"
 }
+resource "azurerm_storage_account" "sa" {
+  name                     = "storageaccou234"
+  resource_group_name      = "${azurerm_resource_group.RG-1.name}"
+  location                 = "${azurerm_resource_group.RG-1.location}"
+  account_tier             = "Standard"
+  account_replication_type = "GRS"
+
+}
